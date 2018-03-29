cb5877f1876754b924fa29c3750f6c732a36aeaa
Hitman <Amrmaxx@gmail.com>
2018-03-29 22:19:08 +0200

    Solution

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        MasterListFragment masterListFragment = new MasterListFragment();
+//
+//
+//        BodyPartFragment headFragment = new BodyPartFragment();
+
+        // Set the list of image id's for the head fragment and set the position to the second image in the list
+//        headFragment.setImageIds(AndroidImageAssets.getHeads());
+//        headFragment.setListIndex(1);
+
+
+//
+//        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+//
+////        fragmentManager.beginTransaction()
+////                .add(R.id.head_container, headFragment)
+////                .commit();
+//
+//        fragmentManager.beginTransaction()
+//                .add(R.id.master_list_container,masterListFragment)
+//                .commit();
+
+
+    }
+}
